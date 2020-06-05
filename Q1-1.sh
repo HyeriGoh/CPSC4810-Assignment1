f180cfabc77a71d40934a0643a481e6d4d1ff6e1
Hyeri Goh <hyeri.goh@gmail.com>
2020-06-05 14:16:55 -0700

    Q2-1, Q2-2

---
+++ Q1-1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+flight = pd.read_csv('flightdelay2007.csv')
+
+
+SFO = flight[flight['Origin']=='SFO']['ArrDelay'].head(3)
+
+print(SFO)
---
+++ Q1-2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+flight = pd.read_csv('flightdelay2007.csv')
+
+count = flight.groupby('Dest').count()
+
+top3 = count.sort_values(by='Year', ascending = False).head(3)
+
+print(top3['Year'])
